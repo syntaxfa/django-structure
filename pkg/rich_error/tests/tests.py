d788db5ffd3ca78195f45314848e5ca4760b240d
alireza-fa <alirezafeyze44@gmail.com>
2025-07-12 11:02:25 +0330

    fix: rich error tests

--- pkg/rich_error/tests/tests.py
+++ pkg/rich_error/tests/tests.py
@@ -10,10 +10,10 @@ class RichErrorTest(TestCase):
         op = "tests.tests.RichErrorTest.test_error_one_layer"
         message = "layer one error occurred"
 
-        err = RichError(operation=op).set_msg(message).set_code(40400)
+        err = RichError(operation=op).set_msg(message).set_code(404_000)
 
         self.assertEqual(str(err), message)
-        self.assertEqual(error_code(error=err), 40400)
+        self.assertEqual(error_code(error=err), 404_000)
 
     def test_error_one_layer_default_variable(self):
         op = "tests.tests.RichErrorTest.test_error_one_layer_default_variable"
@@ -21,7 +21,7 @@ class RichErrorTest(TestCase):
         err = RichError(operation=op)
 
         self.assertEqual(str(err), "")
-        self.assertEqual(error_code(error=err), 500_00)
+        self.assertEqual(error_code(error=err), 500_000)
 
     def test_error_two_layer_message_default(self):
         op = "tests.tests.RichErrorTest.test_error_two_layer_message_default"
@@ -40,7 +40,7 @@ class RichErrorTest(TestCase):
         err2 = RichError(op).set_error(err1)
 
         self.assertEqual(str(err2), "an error")
-        self.assertEqual(error_code(err2), 50000)
+        self.assertEqual(error_code(err2), 500_000)
         self.assertEqual(error_meta(err2), {"user_id": 1})
 
     def test_error_two_layer_message_custom_in_layer_two(self):
@@ -50,7 +50,7 @@ class RichErrorTest(TestCase):
         err2 = RichError(op).set_error(err1).set_msg("an error")
 
         self.assertEqual(str(err2), "an error")
-        self.assertEqual(error_code(err2), 50000)
+        self.assertEqual(error_code(err2), 500_000)
 
     def test_error_three_layer_message_in_layer_one_layer_one_is_exception(self):
         op = "tests.tests.RichErrorTest.test_error_three_layer_message_in_layer_one_layer_one_is_exception"
@@ -60,13 +60,13 @@ class RichErrorTest(TestCase):
         err3 = RichError(op).set_error(err2)
 
         self.assertEqual(str(err3), "an error in exception")
-        self.assertEqual(error_code(err3), 50000)
+        self.assertEqual(error_code(err3), 500_000)
         self.assertEqual(error_meta(err3), {})
 
     def test_error_code_with_exception(self):
         err = Exception()
 
-        self.assertEqual(error_code(err), 50000)
+        self.assertEqual(error_code(err), 500_000)
 
     def test_error_info_with_exception_error(self):
         err = Exception("hello")
@@ -89,25 +89,25 @@ class RichErrorTest(TestCase):
 
     def test_error_info_with_rich_error_value(self):
         op = "tests.tests.RichErrorTest.test_error_info_with_rich_error"
-        err = (RichError(op).set_code(40400).set_error(Exception("exception error")).set_meta({"user_id": 1}).
+        err = (RichError(op).set_code(404_000).set_error(Exception("exception error")).set_meta({"user_id": 1}).
                set_msg("hello from test with rich error"))
         info = _error_info(err)
 
         self.assertEqual(info[constant.OPERATION], "tests.tests.RichErrorTest.test_error_info_with_rich_error")
-        self.assertEqual(info[constant.CODE], 404_00)
+        self.assertEqual(info[constant.CODE], 404_000)
         self.assertEqual(info[constant.MESSAGE], "hello from test with rich error")
         self.assertEqual(info[constant.META], {"user_id": 1})
 
     def test_error_three_layer_is_have_value(self):
         op = "tests.tests.RichErrorTest.test_error_three_layer_is_have_value"
 
-        err1 = RichError(op).set_msg("error 1").set_code(40400).set_meta({"user_id": 1})
-        err2 = RichError(op).set_error(err1).set_msg("error 2").set_code(42900).set_meta({"fullname": "alireza"})
-        err3 = (RichError(op).set_error(err2).set_msg("error 3").set_code(50000).
+        err1 = RichError(op).set_msg("error 1").set_code(404_000).set_meta({"user_id": 1})
+        err2 = RichError(op).set_error(err1).set_msg("error 2").set_code(429_000).set_meta({"fullname": "alireza"})
+        err3 = (RichError(op).set_error(err2).set_msg("error 3").set_code(500_000).
                 set_meta({"user_id": 1, "last_update": "2024"}))
 
         self.assertEqual(str(err3), "error 3")
-        self.assertEqual(error_code(err3), 42900)
+        self.assertEqual(error_code(err3), 429_000)
 
     def test_get_error_info_one_layer_exception(self):
         err = Exception("error")
@@ -145,12 +145,12 @@ class RichErrorTest(TestCase):
         op = "tests.tests.RichErrorTest.test_get_error_info_two_layer_layer_one_exception"
 
         err1 = Exception("error1")
-        err2 = RichError(op).set_error(err1).set_meta({"user_id": 1}).set_code(40400).set_msg("error2")
+        err2 = RichError(op).set_error(err1).set_meta({"user_id": 1}).set_code(404_000).set_msg("error2")
 
         self.assertEqual(get_error_info(err2), [
             {
                 constant.OPERATION: op,
-                constant.CODE: 40400,
+                constant.CODE: 404_000,
                 constant.MESSAGE: "error2",
                 constant.META: {"user_id": 1},
             },
@@ -165,19 +165,19 @@ class RichErrorTest(TestCase):
     def test_get_error_info_two_layer_rich_error(self):
         op = "tests.tests.RichErrorTest.test_get_error_info_two_layer_layer_one_exception"
 
-        err1 = RichError(op).set_msg("error1").set_code(40400)
+        err1 = RichError(op).set_msg("error1").set_code(404_000)
         err2 = RichError(op).set_error(err1).set_meta({"user_id": 1})
 
         self.assertEqual(get_error_info(err2), [
             {
                 constant.OPERATION: op,
-                constant.CODE: 40400,
+                constant.CODE: 404_000,
                 constant.MESSAGE: "error1",
                 constant.META: {"user_id": 1},
             },
             {
                 constant.OPERATION: op,
-                constant.CODE: 40400,
+                constant.CODE: 404_000,
                 constant.MESSAGE: "error1",
                 constant.META: {},
             },
@@ -187,19 +187,19 @@ class RichErrorTest(TestCase):
         op = "tests.tests.RichErrorTest.test_get_error_info_two_layer_layer_one_exception"
 
         err1 = RichError(op).set_msg("error1")
-        err2 = RichError(op).set_error(err1).set_meta({"user_id": 1}).set_code(40300).set_msg("error2")
+        err2 = RichError(op).set_error(err1).set_meta({"user_id": 1}).set_code(403_000).set_msg("error2")
         err3 = RichError(op).set_error(err2).set_meta({"user_id": 1})
 
         self.assertEqual(get_error_info(err3), [
             {
                 constant.OPERATION: op,
-                constant.CODE: 40300,
+                constant.CODE: 403_000,
                 constant.MESSAGE: "error2",
                 constant.META: {"user_id": 1},
             },
             {
                 constant.OPERATION: op,
-                constant.CODE: 40300,
+                constant.CODE: 403_000,
                 constant.MESSAGE: "error2",
                 constant.META: {"user_id": 1},
             },
